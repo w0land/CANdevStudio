b825afd4596a1413984d5a8c94a490df4cc57466
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-06-23 14:47:09 +0200

    wip2

--- tests/config_test.cpp
+++ tests/config_test.cpp
@@ -5,14 +5,26 @@
 
 #include <QDir>
 
-TEST_CASE("Config with path that can't be created", "[config]") { CHECK_THROWS(cds::Config { "/" }); }
-TEST_CASE("Config with current dir", "[config]")
+TEST_CASE("Config with path that can't be created", "[config]") { CHECK_THROWS(cds::Config{ "/" }); }
+TEST_CASE("Non existent config with current dir", "[config]")
 {
     auto d = QDir::current();
     cds::Config c{ d.absolutePath().toStdString() };
-
     CHECK(QFile::exists(d.absolutePath() + "/config"));
+    CHECK(QFile::remove(d.absolutePath() + "/config"));
 }
+
+struct ValidConfigFileTest {
+    ValidConfigFileTest()
+        : _cfg([]() -> std::string {
+            const std::string f = QDir::current().absolutePath().toStdString() + "/config";
+            create_file(f, "{}");
+        }())
+    {
+    }
+
+    cds::Config _cfg;
+};
 
 int main(int argc, char* argv[])
 {
--- tests/testhelpers.hpp
+++ tests/testhelpers.hpp
@@ -4,7 +4,10 @@
 
 #include "log.hpp"
 #include <memory>
+#include <string>
 
 void test_setupLogger();
+
+bool create_file(const std::string& filename, const std::string& data);
 
 #endif /* end of include guard: TESTHELPERS_HPP_TPZ2WHIX */
